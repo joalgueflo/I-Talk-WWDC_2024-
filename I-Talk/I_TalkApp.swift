03834abd2890ba123642b5f0bba0b8c74702c318
joalgueflo <114319054+joalgueflo@users.noreply.github.com>
2024-03-19 09:58:59 -0600

    Initial Commit

---
+++ I-Talk/I_TalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_TalkApp.swift
+//  I-Talk
+//
+//  Created by José Guerrero  on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct I_TalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ I-TalkUITests/I_TalkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  I_TalkUITestsLaunchTests.swift
+//  I-TalkUITests
+//
+//  Created by José Guerrero  on 19/03/24.
+//
+
+import XCTest
+
+final class I_TalkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
